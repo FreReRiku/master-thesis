12317abd27bafff4103aa61f44eb5585ca3675e5
Riku Kasakura <152479589+FreReRiku@users.noreply.github.com>
2025-01-19 04:23:12 +0900

    Modified: main.py

--- src/distance_estimation/main.py
+++ src/distance_estimation/main.py
@@ -14,24 +14,10 @@ music_type = [1, 2]         # 使用する楽曲のタイプ
 
 for music in music_type:
 
-    calc.gcc_phat(music_type=music)
+    calc.gcc_phat(music)
     
-    output_path = f'./../../data/distance_estimation/music{music}_mono/csv_files'       # 保存するCSVファイルのパス
-
-    visualize.plot_embedding_error(
-        music_type=music,
-        delay_time_errors_file=f'{output_path}/delay_time_errors.csv',
-        pesq_scores_file=f'{output_path}/pesq_scores.csv',
-        embedding_amplitudes=np.linspace(0, 1, 25),
-    )
-
-    visualize.plot_impulse(
-        num='Impulse',
-        figsize=(6, 3),
-        impulse_position_data_path=f'{output_path}/first_detected_peak_positions.csv',
-        fs=44100,
-        impulse_response_path=f'{output_path}/impulse.csv',
-        fft_points=1024
-    )
+    visualize.plot_embedding_error(music)
+    visualize.plot_impulse(music)
+    visualize.plot_mean_embedded_csp(music)
 
 print("Completed!!!")
