4d378c17b0d98a44aea2749d67aa600764038749
Riku Kasakura <152479589+FreReRiku@users.noreply.github.com>
2025-01-30 04:54:40 +0900

    Modified: main.py

--- src/distance_estimation/main.py
+++ src/distance_estimation/main.py
@@ -10,16 +10,17 @@ import numpy as np
 import visualize
 
 music_type = [1, 2]         # 使用する楽曲のタイプ
-
+emb_type = ["amp", "phase"]
 
 for music in music_type:
 
-    calc.gcc_phat(music)
-    
-    visualize.plot_embedding_error(music)
-    visualize.plot_impulse(music)
-    visualize.plot_mean_embedded_csp(music)
-    visualize.plot_embedded_frequencies(music)
-    visualize.plot_audio_waveform(music)
+    for emb in emb_type:
+
+        calc.gcc_phat(music, emb)
+        visualize.plot_embedding_error(music, emb)
+        visualize.plot_impulse(music, emb)
+        visualize.plot_mean_embedded_csp(music, emb)
+        visualize.plot_embedded_frequencies(music, emb)
+        visualize.plot_audio_waveform(music, emb)
 
 print("Completed!!!")
