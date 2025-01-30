230edd339191ba4734d9272f2d31cb0ca51458c6
Riku Kasakura <152479589+FreReRiku@users.noreply.github.com>
2025-01-30 19:13:23 +0900

    Modified: main.py

--- src/distance_estimation/main.py
+++ src/distance_estimation/main.py
@@ -14,8 +14,10 @@ emb_type = ["amplitude_modulation", "phase_modulation"]
 for music in music_type:
 
     for emb in emb_type:
-
+        # 遅延推定の計算
         calc.gcc_phat(music, emb)
+
+        # 可視化処理
         visualize.plot_embedding_error(music, emb)
         visualize.plot_impulse(music, emb)
         visualize.plot_mean_embedded_csp(music, emb)
